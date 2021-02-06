353ffa39cf09fbde5aa393e3f7e363fef32bc73d
Mahrs <mahrsbar22@gmail.com>
2021-02-06 02:43:29 -0700

    fix padding

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
