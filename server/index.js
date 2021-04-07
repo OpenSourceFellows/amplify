74b4a2a744bd148a14a97d3451432565dd4cd957
Manisha Priyadarshini <manishapriya94@github.com>
2021-04-07 13:23:50 -0700

    Update index.js
    added auth page from this issue https://github.com/Ally-Guide/amplify-back-end/issues/15

--- server/index.js
+++ server/index.js
@@ -9,11 +9,13 @@ const app = express();
 app.use(cors());
 
 const amplify = require('./routes/api/amplify');
+const auth = require('.routes/api/auth');
 // const give = require('./routes/api/give');
 // const library = require('./routes/api/library');
 // const email = require('./routes/api/email')
 
 app.use('/api/amplify', amplify);
+app.use('/api/auth', auth);
 // app.use('/api/give', give);
 // app.use('/api/library', library);
 // app.use('/api/email', email);
