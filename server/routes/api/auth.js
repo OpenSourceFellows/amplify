49334534c01df79bff7ebcaeb56868633f180b96
Travis Kopp <teakopp@github.com>
2021-04-09 10:27:33 -0700

    added auth to /api/amplify

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -2,3 +2,17 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 require('dotenv').config()
+const { auth } = require('express-openid-connect');
+
+app.use(
+  auth({
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+  })
+);
+
+module.exports = app;
+
