4d0d610b64aab04c73fd1e8b1c500b517f04eb3d
Travis Kopp <teakopp@github.com>
2021-05-06 11:54:20 -0700

    adds authRequired option to routes

--- server/app.js
+++ server/app.js
@@ -6,11 +6,7 @@ const { auth } = require('express-openid-connect')
 
 app.use(
     auth({
-        issuerBaseURL: process.env.ISSUER_BASE_URL,
-        baseURL: process.env.BASE_URL,
-        clientID: process.env.CLIENT_ID,
-        secret: process.env.SECRET,
-        idpLogout: true,
+        authRequired: false,
     })
 )
 
