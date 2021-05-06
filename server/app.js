bbdfc173da09eb3d14fd3994237d5d3079d2169a
Travis Kopp <teakopp@github.com>
2021-05-06 11:52:25 -0700

    remove auth.js and add auth to app.js

--- server/app.js
+++ server/app.js
@@ -1,20 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { auth } = require('express-openid-connect')
+
+app.use(
+    auth({
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+        idpLogout: true,
+    })
+)
 
 //Middleware
 //app.use(express.json());
-app.use(cors());
+app.use(cors())
 
-const amplify = require('./routes/api/amplify');
+const amplify = require('./routes/api/amplify')
 // const give = require('./routes/api/give');
 
 // const email = require('./routes/api/email')
 
-app.use('/api/amplify', amplify);
+app.use('/api/amplify', amplify)
 // app.use('/api/give', give);
 // app.use('/api/library', library);
 // app.use('/api/email', email);
 
-module.exports = app;
+module.exports = app
