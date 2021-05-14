4bc34615a8bc2b3d4a43f657304453a5a81b22b5
James M. Greene <jamesmgreene@github.com>
2021-05-14 15:05:36 -0500

    Rearrange columns and add special CHECK constraints

--- server/db/migrations/20210514145630_create-letter-versions-table.js
+++ server/db/migrations/20210514145630_create-letter-versions-table.js
@@ -11,9 +11,10 @@ module.exports = {
 
         // Simple fields
         table.string('template_id').notNullable()
-        table.string('state')
-        table.string('county')
-        table.string('municipality')
+
+        // Foreign key references
+        table.integer('campaign_id').unsigned().notNullable()
+        table.foreign('campaign_id').references('campaigns.id')
 
         // Fields using native enum types
         table
@@ -21,12 +22,14 @@ module.exports = {
             'office_division',
             ['Federal', 'State', 'County', 'Municipality'],
             { useNative: true, enumName: 'political_division' }
-          ).notNullable()
+          )
+          .notNullable()
           .defaultTo('Federal')
 
-        // Foreign key references
-        table.integer('campaign_id').unsigned().notNullable()
-        table.foreign('campaign_id').references('campaigns.id')
+        // More simple fields
+        table.string('state')
+        table.string('county')
+        table.string('municipality')
 
         // Indexes
         table.index(['campaign_id'])
@@ -34,10 +37,81 @@ module.exports = {
         // Unique indexes
         table.unique(['template_id'])
       })
+
+    //
+    // Special CHECK constraints to verify row integrity
+    //
+
+    // If "office_division" is set to 'Federal', then:
+    //  - the value for "state" must be NULL
+    //  - the value for "county" must be NULL
+    //  - the value for "municipality" must be NULL
+    await knex.raw(
+      `
+      ALTER TABLE "${tableName}"
+      ADD CONSTRAINT "federal_division_has_no_locations"
+      CHECK (
+        office_division = 'Federal' AND
+        state IS NULL AND
+        county IS NULL AND
+        municipality IS NULL
+      );
+      `
+    )
+
+    // If "office_division" is set to 'State', then:
+    //  - the value for "state" must NOT be NULL
+    //  - the value for "county" must be NULL
+    //  - the value for "municipality" must be NULL
+    await knex.raw(
+      `
+      ALTER TABLE "${tableName}"
+      ADD CONSTRAINT "state_division_has_required_locations"
+      CHECK (
+        office_division = 'State' AND
+        state IS NOT NULL AND
+        county IS NULL AND
+        municipality IS NULL
+      );
+      `
+    )
+
+    // If "office_division" is set to 'County', then:
+    //  - the value for "state" must NOT be NULL
+    //  - the value for "county" must NOT be NULL
+    //  - the value for "municipality" must be NULL
+    await knex.raw(
+      `
+      ALTER TABLE "${tableName}"
+      ADD CONSTRAINT "county_division_has_required_locations"
+      CHECK (
+        office_division = 'County' AND
+        state IS NOT NULL AND
+        county IS NOT NULL AND
+        municipality IS NULL
+      );
+      `
+    )
+
+    // If "office_division" is set to 'Municipality', then:
+    //  - the value for "state" must NOT be NULL
+    //  - the value for "county" may be NULL? 🤷
+    //  - the value for "municipality" must NOT be NULL
+    await knex.raw(
+      `
+      ALTER TABLE "${tableName}"
+      ADD CONSTRAINT "municipality_division_has_required_locations"
+      CHECK (
+        office_division = 'Municipality' AND
+        state IS NOT NULL AND
+        municipality IS NOT NULL
+      );
+      `
+    )
   },
 
   async down (knex) {
-    // Drop the table
+    // Drop the table (and its special CHECK constraints)
     await knex.schema.dropTable(tableName)
 
     // Manually remove the native enum types
