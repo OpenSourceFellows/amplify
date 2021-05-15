c38cc9de50f5b31f10504d12e53fc332821ac1ec
BeeXiong <beexiong@github.com>
2021-05-15 15:56:54 -0600

    campaigns/:id route added

---
+++ server/routes/api/campaigns.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const axios = require('axios')
+const { createClient } = require('../../db')
+const router = express.Router()
+const db = createClient()
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await db('campaigns').where('id',id);
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: 'Whoops' });
+    }
+})
+
+module.exports = router
