617e86e4383e554022050e69511d765be697de3e
Nawaz Khan <nawazahamedkhan@gmail.com>
2021-05-21 23:29:08 -0700

    Revert "Revert "Displays rep name and creates a new rep card component""

---
+++ src/components/RepresentativeCard.vue
@@ -0,0 +1,31 @@
+// PARENT
+<template lang="html">
+    <v-card v-on:click="handleRepClick(member)">
+        <v-card-title v-text="member.name"></v-card-title>
+        <v-card-subtitle v-text="member.title" style="text-align:left">
+        </v-card-subtitle>
+        <v-card-subtitle
+            v-text="member.city"
+            style="text-align:left"
+        ></v-card-subtitle>
+    </v-card>
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'representative-card',
+    components: {
+    },
+    props: ['member', 'handleRepClick'],
+    mounted () {},
+    data () {
+      return {}
+    },
+    methods: {},
+}
+</script>
+
+<style scoped lang="less">
+.search-reps {
+}
+</style>
