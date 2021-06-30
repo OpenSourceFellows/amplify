7e4b77c621b52dc3d7eb0b2a15268009aaa7549c
BeeXiong <beexiong@github.com>
2021-06-29 20:59:12 -0600

    remove try catch

--- server/routes/api/lob.js
+++ server/routes/api/lob.js
@@ -8,22 +8,20 @@ router.get('/:template_id', async (req, res) => {
     const template_id = req.params.template_id;
     var templateinfo = {};
 
-    try {
-        axios
-        .get('https://api.lob.com/v1/templates/'+template_id, {
-            auth: {
-                username: process.env.LiveLob,      
-            },
-        })
-        .then(function (response) {
-            templateinfo = response.data
-            res.send(templateinfo);
-        })
-        
-    } catch (error) {
-        console.log(error);
+    axios
+    .get('https://api.lob.com/v1/templates/'+template_id, {
+        auth: {
+            username: process.env.LiveLob,      
+        },
+    })
+    .then(function (response) {
+        templateinfo = response.data
+        res.send(templateinfo);
+    })
+    .catch(function (error) {
+        console.log(error)
         res.status(500).send({ error: 'Whoops' });
-    }
+    })
 });
 
 module.exports = router
