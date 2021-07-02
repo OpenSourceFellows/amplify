062ae6fee428c5b70fa11fac5493b6e88b2f0fb7
Mahrs <mahrsbar22@gmail.com>
2021-07-01 20:12:35 -0600

    trying to fix sourcetree

--- src/components/SearchReps.vue
+++ src/components/SearchReps.vue
@@ -85,6 +85,7 @@ import axios from 'axios';
 
                 let latestVersion = versions.data[versions.data.length - 1].template_id;
 
+
                 const letter = await axios.get(
                     'https://murmuring-headland-63935.herokuapp.com/api/lob/'+ latestVersion
                     );
