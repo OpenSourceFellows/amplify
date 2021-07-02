20537d193f89e166584be4bda0bc375c2e19d641
Mahrs A <>
2021-07-01 20:02:09 -0600

    test push

--- src/components/SearchReps.vue
+++ src/components/SearchReps.vue
@@ -93,7 +93,6 @@ import axios from 'axios';
                     'https://murmuring-headland-63935.herokuapp.com/api/lob/'+ latestVersion
                     );
         
-               
                 this.letterBody = letter.data.versions[0].html;
 
 
