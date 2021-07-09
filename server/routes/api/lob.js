9b3e2840180874f8c43b9b4c26d00389b2dc91ad
James M. Greene <jamesmgreene@github.com>
2021-07-08 20:54:42 -0500

    Add a new endpoint 'POST /api/lob/addressVerification'
    Towards https://github.com/Ally-Guide/amplify-back-end/issues/36

--- server/routes/api/lob.js
+++ server/routes/api/lob.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const axios = require('axios')
 const { createClient } = require('../../db')
+const Lob = require('lob')
+
 const router = express.Router()
 const db = createClient()
 
 const LOB_API_KEY = getLobApiKey()
+const lob = new Lob({ apiKey: LOB_API_KEY })
+
+const ALLOWED_ADDRESS_FIELDS = ['line1', 'line2', 'city', 'state', 'zip']
+const VALID_US_ZIP_CODE_MATCH = /^(?:\d{1,4}|\d{5}(?:[+-]?\d{4})?)$/
+const DELIVERABILITY_WARNINGS = {
+    undeliverable: 'Address is not deliverable',
+    deliverable_incorrect_unit: 'Address may be deliverable but contains a suite number that does not exist',
+    deliverable_missing_unit: 'Address may be deliverable but is missing a suite number',
+    deliverable_unnecessary_unit: 'Address may be deliverable but contains an unnecessary suite number'
+}
 
 router.get('/:templateId', async (req, res) => {
     const { templateId } = req.params
@@ -26,6 +38,104 @@ router.get('/:templateId', async (req, res) => {
         return res.status(200).send(templateInfo)
     }
     catch (error) {
+        return handleLobError(error, res)
+    }
+})
+
+router.post('/addressVerification', async (req, res) => {
+    const address = req.body
+
+    // Very rough schema validation
+    try {
+        const keys = Object.keys(address || {}).sort()
+        if (!address || keys.length === 0) {
+            throw new Error('Address object cannot be empty')
+        }
+
+        const disallowedKeys = keys.reduce(
+            (badKeys, key) => {
+                if (!ALLOWED_ADDRESS_FIELDS.includes(key)) {
+                    badKeys.push(key)
+                }
+                return badKeys
+            },
+            []
+        )
+
+        if (disallowedKeys.length > 0) {
+            throw new Error(`Address object contained unexpected keys: ${JSON.stringify(disallowedKeys)}`)
+        }
+
+        if (!(address.line1 || '').trim()) {
+            throw new Error('Address object must contain a primary line (line1)')
+        }
+
+        const { zip } = address
+        if (zip != null && typeof zip !== 'string') {
+            throw new Error('Address object must contain a string-based ZIP code')
+        }
+
+        let zipCode = (zip || '').trim()
+        if (zipCode) {
+            if (!VALID_US_ZIP_CODE_MATCH.test(zipCode)) {
+                throw new Error(`Address object contained an invalid ZIP code: ${zipCode}`)
+            }
+        } else if (!((address.city || '').trim() && (address.state || '').trim())) {
+            throw new Error('Address object must include both city and state, or a ZIP code')
+        }
+    } catch (validationError) {
+        return res.status(400).send({ error: validationError.message })
+    }
+
+    const { line1, line2, city, state, zip } = address
+    // Ensure the ZIP code is at least 5 digits
+    const zipCode = zip ? zip.padStart(5, '0') : null
+
+    try {
+        const response = await lob.usVerifications.verify({
+            primary_line: line1,
+            secondary_line: line2,
+            city,
+            state,
+            zip_code: zipCode
+        })
+
+        const {
+            deliverability,
+            components: {
+                state: stateAbbr,
+                address_type: addressType,
+                record_type: recordType
+            }
+        } = response.data
+
+        const undeliverable = !deliverability || deliverability === 'undeliverable'
+        const isResidential = addressType === 'residential'
+        const isPostOfficeBox = recordType === 'po_box'
+        const isPuertoRico = stateAbbr === 'PR'
+
+        const deliverable = !undeliverable && isResidential && !isPostOfficeBox && !isPuertoRico
+        const warning = DELIVERABILITY_WARNINGS[deliverability] || null
+
+        if (!deliverable) {
+            let errorMessage = null
+            if (!isResidential) {
+                errorMessage = 'Non-residential addresses are not currently supported'
+            } else if (isPostOfficeBox) {
+                errorMessage = 'Post office boxes are not currently supported'
+            } else if (isPuertoRico) {
+                errorMessage = 'Puerto Rico addresses are not currently supported'
+            } else {
+                errorMessage = 'Address is undeliverable'
+            }
+
+            return res.status(400).send({ error: errorMessage })
+        }
+
+        return res.status(200).send({ deliverable, warning })
+    } catch (error) {
+        // This endpoint should not return anything other than `200` status
+        // codes, even for undeliverable addresses
         return handleLobError(error, res)
     }
 })
