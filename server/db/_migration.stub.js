73602b463bed65c4586a0eaa3e289def2795d491
James M. Greene <jamesmgreene@github.com>
2021-07-14 22:07:19 -0500

    ESLint fix: use the knex variables in the migration stub

--- server/db/_migration.stub.js
+++ server/db/_migration.stub.js
@@ -1,11 +1,9 @@
 module.exports = {
-
-  async up (knex) {
-    // await knex...
+  async up(knex) {
+    await knex.schema.doSomethingForReal()
   },
 
-  async down (knex) {
-    // await knex...
-  }
-
+  async down(knex) {
+    await knex.schema.doSomethingForReal()
+  },
 }
