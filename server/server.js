447fb2477b7682447cdab019e8980c17c33ed4b5
Manisha Priyadarshini <manishapriya94@github.com>
2021-08-05 13:37:57 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public'))
 
   //Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-}
+  app.get(/.*/, (req, res) => {
+   res.send('Hello World!')
+})
 
 const port = process.env.PORT || 5000
 
