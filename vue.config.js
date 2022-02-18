dcf1096b87bb85573afc86109e6e46bd7283ba79
James M. Greene <JamesMGreene@github.com>
2022-02-17 21:36:42 -0600

    Adjust Vue build config and static serving to match expectations

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, './server/views'),
+  outputDir: path.resolve(__dirname, './dist'),
 
   devServer: {
     host: 'localhost'
