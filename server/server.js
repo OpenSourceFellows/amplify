11bd0f46f6469e7587af725fae4e5c920fe69b3e
James M. Greene <JamesMGreene@github.com>
2022-02-17 19:30:20 -0600

    Fix formatting complaint

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const DEPLOYED_PORT = process.env.PORT
 const viewPort = process.env.VIEWPORT || 8080
 const apiPort = DEPLOYED_PORT || process.env.APIPORT || 6000
 
-
 if (!DEPLOYED_PORT) {
   web.listen(viewPort, () =>
     console.log(`Web server started on port ${viewPort}`)
