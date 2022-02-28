b695c70f021185406bfb16fc21ad292eed33cd0d
Edwin Chong <8726446-ewchong@users.noreply.gitlab.com>
2022-02-28 07:02:20 -0500

    Switch backend to use Stripe Checkout

--- server/routes/api/checkout.js
+++ server/routes/api/checkout.js
@@ -35,9 +35,11 @@ router.post('/create-transaction', async (req, res) => {
 // 1. send a request to `/create-payment-intent`
 // with a `donationAmount` as string or integer
 // If user doesn't select any particular `donationAmount`, send `1` in the donationAmount
-// 2. This API will return the client secret. Use it to complete the transaction in the UI
+// 2. This API will redirect the client to a Stripe Checkout page
+// 3. Once user completes payment, will redirect back to `success_url` with
+//  a Stripe session_id included in the URL.
 
-router.post('/create-payment-intent', async (req, res) => {
+router.post('/create-checkout-session', async (req, res) => {
   try {
     const acceptableCharges = [1, 2, 20, 50]
     const { donationAmount } = req.body || {}
@@ -47,13 +49,27 @@ router.post('/create-payment-intent', async (req, res) => {
       return res.status(400).send({ error: 'Invalid Amount' })
     }
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: parsedDonationAmount * 100, // in cents
-      currency: 'usd'
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Donation'
+            },
+            unit_amount: parsedDonationAmount * 100
+          },
+          quantity: 1
+        }
+      ],
+      mode: 'payment',
+      allow_promotion_codes: true,
+      success_url:
+        'http://localhost:8080/about?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://localhost:8080'
     })
-    res.send({
-      clientSecret: paymentIntent.client_secret
-    })
+
+    res.redirect(303, session.url)
   } catch (error) {
     console.log({ error })
   }
