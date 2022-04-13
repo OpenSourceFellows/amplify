5130750205416594436a88a4b1f9c658fb9585e5
James M. Greene <JamesMGreene@github.com>
2022-04-13 23:54:03 +0000

    Expand Campaign model's jsonSchema a bit

--- server/db/models/campaign.js
+++ server/db/models/campaign.js
@@ -7,22 +7,39 @@ class Campaign extends BaseModel {
 
   // Optional JSON schema. This is not the database schema! Nothing is generated
   // based on this. This is only used for validation. Whenever a model instance
-  // is created it is checked against this schema. http://json-schema.org/.
+  // is created, it is checked against this schema. http://json-schema.org/
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['name'],
+      required: ['organization', 'name', 'cause', 'type', 'page_url'],
 
       properties: {
         id: { type: 'integer' },
         organization: { type: 'string', minLength: 1, maxLength: 255 },
         name: { type: 'string', minLength: 1, maxLength: 255 },
-        cause: { type: 'string', minLength: 1, maxLength: 255 },
-        type: { type: 'string', minLength: 1, maxLength: 255 },
-        page_url: { type: 'string', minLength: 1, maxLength: 255 },
-        letters_counter: { type: 'integer' }
+        cause: {
+          type: 'string',
+          enum: ['Civic Rights', 'Education', 'Climate Justice']
+        },
+        type: { type: 'string', enum: ['Starter', 'Accelerator', 'Grant'] },
+        page_url: { type: 'string', minLength: 1 },
+        logo_url: { type: 'string', minLength: 1 },
+        // TODO: Move `letters_counter` to be a mapped relation
+        letters_counter: { type: 'integer', minimum: 0 },
+        letters_goal: {
+          anyOf: [{ type: 'integer', minimum: 0 }, { type: 'null' }]
+        },
+        donation_goal: {
+          anyOf: [{ type: 'integer', minimum: 0 }, { type: 'null' }]
+        }
       }
     }
+  }
+
+  // This object defines the relations to other models.
+  static get relationMappings() {
+    // TODO: Implement this once additional related models classes are defined
+    return {}
   }
 }
 
