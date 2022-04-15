3ad9e9fa48f3bb57deb9554e7db9ed8535b3f9fe
James M. Greene <JamesMGreene@github.com>
2022-04-15 21:58:06 +0000

    Use the Models to retrieve LetterVersions

--- server/routes/api/letter_versions.js
+++ server/routes/api/letter_versions.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { createClient } = require('../../db')
 const router = express.Router()
-const db = createClient()
+const Campaign = require('../../db/models/campaign')
+//const LetterVersion = require('../../db/models/letter-version')
 
 router.get('/:campaignId', async (req, res) => {
   const campaignId = req.params.campaignId
   try {
-    const result = await db('letter_versions').where('campaign_id', campaignId)
+    const result = await Campaign.relatedQuery('LetterVersions').for(campaignId)
+    //const result = await LetterVersion.query().where('campaign_id', campaignId)
     res.send(result)
   } catch (error) {
     console.log(error)
