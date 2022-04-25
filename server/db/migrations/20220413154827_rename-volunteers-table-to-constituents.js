22fdda6a9f6824dd5153ee7bb24b1f3f3a55a923
James M. Greene <JamesMGreene@github.com>
2022-04-25 03:51:39 +0000

    Update old migration for consistency

--- server/db/migrations/20220413154827_rename-volunteers-table-to-constituents.js
+++ server/db/migrations/20220413154827_rename-volunteers-table-to-constituents.js
@@ -5,7 +5,7 @@ const newTableName = 'constituents'
 module.exports = {
   async up(knex) {
     // Step 1: Unbind the referencing table
-    await knex.schema.table(referencingTableName, (table) => {
+    await knex.schema.alterTable(referencingTableName, (table) => {
       table.dropIndex(['volunteer_id'])
       table.dropForeign('volunteer_id')
     })
@@ -14,7 +14,7 @@ module.exports = {
     await knex.schema.renameTable(oldTableName, newTableName)
 
     // Step 3: Rebind the referencing table
-    await knex.schema.table(referencingTableName, (table) => {
+    await knex.schema.alterTable(referencingTableName, (table) => {
       table.renameColumn('volunteer_id', 'constituent_id')
       table.foreign('constituent_id').references(`${newTableName}.id`)
       table.index(['constituent_id'])
@@ -23,7 +23,7 @@ module.exports = {
 
   async down(knex) {
     // Step 1: Unbind the referencing table
-    await knex.schema.table(referencingTableName, (table) => {
+    await knex.schema.alterTable(referencingTableName, (table) => {
       table.dropIndex(['constituent_id'])
       table.dropForeign('constituent_id')
     })
@@ -32,7 +32,7 @@ module.exports = {
     await knex.schema.renameTable(newTableName, oldTableName)
 
     // Step 3: Rebind the referencing table
-    await knex.schema.table(referencingTableName, (table) => {
+    await knex.schema.alterTable(referencingTableName, (table) => {
       table.renameColumn('constituent_id', 'volunteer_id')
       table.foreign('volunteer_id').references(`${oldTableName}.id`)
       table.index(['volunteer_id'])
