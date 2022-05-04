4187cbdf302ed11aac4f1bd7e666f516f8318d66
Mahrs <mahrsbar22@gmail.com>
2022-05-04 02:45:56 +0000

    implement eds changes

--- server/routes/api/campaigns.js
+++ server/routes/api/campaigns.js
@@ -1,28 +1,81 @@
+/* eslint-disable no-unused-vars */
+
 const express = require('express')
+const { createClient } = require('../../db')
 const router = express.Router()
-const Campaign = require('../../db/models/campaign')
+const db = createClient()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-router.get('/:id', async (req, res) => {
-  const id = req.params.id
+router.post('/create-transaction', async (req, res) => {
+  const { session_id/*,  email, campaignId, donationId */} = req.body || {}
+  if (!session_id /*|| !email*/) {
+    return res.status(400).send({ error: 'No session ID' })
+  }
+  const session = await stripe.checkout.sessions.retrieve(session_id);
+  const customer = await stripe.customers.retrieve(session.customer);
+
+  const formattedTransaction = {
+    stripe_transaction_id: session_id,
+    amount: session.amount_total,
+    currency: session.currency,
+    payment_method: "something not empty", // Not sure what this is for
+    payment_method_type: session.payment_method_types[0],
+    email: session.customer_details.email // to-do: get user email from the server auth, if possible
+  }
+
   try {
-    const campaign = await Campaign.query().findById(id)
-    console.log(campaign)
-    res.send(campaign)
+    // Expire session?
+    await db('transactions').insert(formattedTransaction)
+    res.status(200).send(formattedTransaction)
   } catch (error) {
-    console.log(error)
-    res.status(500).send({ error: 'Whoops' })
+    console.log({ error })
+    res.status(400).send()
+  }
+});
+
+// 1. send a request to `/create-payment-intent`
+// with a `donationAmount` as string or integer
+// If user doesn't select any particular `donationAmount`, send `1` in the donationAmount
+// 2. This API will redirect the client to a Stripe Checkout page
+// 3. Once user completes payment, will redirect back to `success_url` with
+//  a Stripe session_id included in the URL.
+
+router.post('/create-checkout-session', async (req, res) => {
+  try {
+    const acceptableCharges = [1, 2, 20, 50]
+    const { donationAmount } = req.body || {}
+    const parsedDonationAmount = parseInt(donationAmount, 10)
+
+    if (!acceptableCharges.includes(parsedDonationAmount)) {
+      return res.status(400).send({ error: 'Invalid Amount' })
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Donation'
+            },
+            unit_amount: parsedDonationAmount * 100
+          },
+          quantity: 1
+        }
+      ],
+      mode: 'payment',
+      allow_promotion_codes: true,
+      success_url:
+        'http://localhost:8080/complete?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'http://localhost:8080'
+    })
+
+    res.redirect(303, session.url)
+  } catch (error) {
+    console.log({ error })
   }
 })
 
-router.get('/', async (req, res) => {
-  try {
-    const campaigns = await Campaign.query()
-    console.log(campaigns)
-    res.send(campaigns)
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({ error: 'Whoops' })
-  }
-})
+
 
 module.exports = router
--- src/components/ActionComplete.vue
+++ src/components/ActionComplete.vue
@@ -1,28 +1,41 @@
 <template lang="html">
   <section class="action-complete">
     <h1>action-complete Component</h1>
+    <h1>Thank you {{ data.email }} for your ${{ data.amount * 0.01 }} donation!</h1>
   </section>
 </template>
 
 <script lang="js">
-
+import axios from 'axios'
 export default {
     name: 'ActionComplete',
     props: [],
     data () {
         return {
-
+          data: {
+            email: null,
+            amount: null
+                }
         }
     },
     computed: {
-
     },
     mounted () {
-
+        this.amount = this.getSession()
     },
     methods: {
-
-    }
+        getSession() {
+            axios.defaults.baseURL = '//localhost:6000/';
+            const session_id = {'session_id': this.$route.query.session_id }
+            axios.post( '/api/checkout/create-transaction', session_id )
+                .then((response) => {
+                    this.data  = response.data
+                })
+                .catch(function (error) {
+                   console.log(error)
+               })
+         },
+   }
 }
 </script>
 
