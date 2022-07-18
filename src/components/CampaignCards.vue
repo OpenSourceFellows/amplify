b5fb7ba50e91407d118f2752a5958ef66edaea64
Mariah Morales <72050998+mariah622@users.noreply.github.com>
2022-07-18 23:21:37 +0000

    word wrapping fix using vuetify built in classes

--- src/components/CampaignCards.vue
+++ src/components/CampaignCards.vue
@@ -9,8 +9,7 @@
             height="200px"
           ></v-img>
 
-          <v-card-title style="word-break: break-word" v-text="campaign.name">
-          </v-card-title>
+          <h5 v-text="campaign.name" class="text-h5"></h5>
 
           <v-card-subtitle
             v-text="campaign.organization"
