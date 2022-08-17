4e5dc65a64616fbf969a6549cd2b1a2e4b8cd92a
Glenn Piludu <54147446+DietBepis1@users.noreply.github.com>
2022-08-17 18:34:50 +0000

    Fixed merge conflict

--- src/components/SearchReps.vue
+++ src/components/SearchReps.vue
@@ -4,9 +4,7 @@
       <v-col>
         <v-card flat>
           <v-card-text>
-            <v-subheader class="pa-0">
-              Where do you live?
-            </v-subheader>
+            <v-subheader class="pa-0"> Where do you live? </v-subheader>
 
             <v-form ref="form">
               <v-text-field
@@ -30,16 +28,9 @@
           </v-card-text>
         </v-card>
 
-        <div
-          v-show="hasContent"
-          id="reprenstatives-list"
-        >
+        <div v-show="hasContent" id="reprenstatives-list">
           <div>
-            <v-card
-              v-for="member in congressMembers"
-              :key="member.name"
-              flat
-            >
+            <v-card v-for="member in congressMembers" :key="member.name" flat>
               <representative-card
                 :member="member"
                 @handleRepSelected="handleRepSelected"
@@ -57,7 +48,7 @@
               alt="Vue logo"
               src="../assets/images/StepsGraphic.png"
               width="70%"
-            >
+            />
           </div>
           <p class="text-h6 pa-10">
             The bill establishes an interim goal to reduce greenhouse gas
@@ -68,10 +59,7 @@
         </div>
 
         <div v-else>
-          <take-action
-            :letter-body="letterBody"
-            :selected-rep="selectedRep"
-          />
+          <take-action :letter-body="letterBody" :selected-rep="selectedRep" />
         </div>
       </v-col>
     </v-row>
@@ -94,9 +82,11 @@ export default {
         letterBody: '',
         selectedRep: {},
         congressMembers: [],
+        currentFilter: '',
         hasContent: true,
         postalCode: this.$route.params.postalCode || '',
         listVisible: false,
+        isActive: false,
       }
     },
     methods: {
@@ -118,15 +108,49 @@ export default {
             this.$store.commit('setGenericValue', { key: 'zipcode', value: this.postalCode })
 
             const res = await axios.get(
-              '/api/representatives/' + this.postalCode
-            )
-            this.congressMembers = res.data
-            this.hasContent = true
-            console.log(res.data)
-            this.listVisible=true
-          } catch (e) {
-            console.error(e)
-          }
+                    '/api/representatives/' + this.postalCode
+                )
+                this.isActive = false
+
+                this.congressMembers = res.data
+                this.hasContent = true
+                // console.log(res.data)
+                this.listVisible = true
+            } catch (e) {
+                console.error(e)
+            }
+        },
+        async FilterList (level) {
+            try {
+                this.currentFilter = level
+
+                if (!this.isActive) {
+                    this.isActive = true
+                    const params = {}
+                    if (this.currentFilter != null) {
+                        params.filter = this.currentFilter
+                    }
+
+                    const res = await axios.get(
+                        '/api/representatives/' + this.postalCode,
+                        {
+                          params
+                        }
+                    )
+
+                    console.log(res)
+                    this.congressMembers = res.data
+                } else {
+                    this.isActive = false;
+                    const res = await axios.get(
+                        '/api/representatives/' + this.postalCode
+                    )
+
+                    this.congressMembers = res.data
+                }
+            } catch (e) {
+                console.error(e)
+            }
         }
     }
 }
