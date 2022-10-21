75ab4550401a1b949635810e6b76b6f33d6ff24b
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-10-21 15:12:09 +0000

    searchreps: responsive

--- src/components/SearchReps.vue
+++ src/components/SearchReps.vue
@@ -1,134 +1,142 @@
 <template lang="html">
   <section class="search-reps">
-    <v-row class="pa-14">
-      <v-col>
-        <v-card flat>
-          <v-card-text>
-            <v-subheader class="pa-0"> Where do you live? </v-subheader>
+    <v-container fluid>
+      <v-row class="justify-center">
+        <v-col cols="12" sm="6">
+          <v-card flat>
+            <v-card-text>
+              <v-subheader class="pa-0"> Where do you live? </v-subheader>
 
-            <v-form ref="form">
-              <v-text-field
-                label="Postal Code"
-                required
-                v-on:keyup="CheckInputContent"
-                v-model="postalCode"
-              ></v-text-field>
-            </v-form>
+              <v-form ref="form">
+                <v-text-field
+                  label="Postal Code"
+                  required
+                  v-on:keyup="CheckInputContent"
+                  v-model="postalCode"
+                ></v-text-field>
+              </v-form>
 
-            <v-row>
-              <v-col>
-                <v-btn
-                  class="search-reps-button"
-                  rounded
-                  dark
-                  :style="{
-                    backgroundColor:
-                      currentFilter === 'federal' && isActive ? 'blue' : 'gray'
-                  }"
-                  v-on:click="FilterList('federal')"
+              <v-row>
+                <v-col>
+                  <v-btn
+                    class="search-reps-button"
+                    rounded
+                    dark
+                    :style="{
+                      backgroundColor:
+                        currentFilter === 'federal' && isActive
+                          ? 'blue'
+                          : 'gray'
+                    }"
+                    v-on:click="FilterList('federal')"
+                  >
+                    Federal
+                  </v-btn>
+
+                  <v-btn
+                    class="search-reps-button"
+                    rounded
+                    dark
+                    :class="{ active: isActive }"
+                    :style="{
+                      backgroundColor:
+                        currentFilter === 'state' && isActive ? 'blue' : 'gray'
+                    }"
+                    v-on:click="FilterList('state')"
+                  >
+                    State
+                  </v-btn>
+
+                  <v-btn
+                    class="search-reps-button"
+                    rounded
+                    dark
+                    :class="{ active: isActive }"
+                    :style="{
+                      backgroundColor:
+                        currentFilter === 'county' && isActive ? 'blue' : 'gray'
+                    }"
+                    v-on:click="FilterList('county')"
+                  >
+                    County
+                  </v-btn>
+
+                  <v-btn
+                    rounded
+                    dark
+                    class="ui button toggle search-reps-button"
+                    :style="{
+                      backgroundColor:
+                        currentFilter === 'municipality' && isActive
+                          ? 'blue'
+                          : 'gray'
+                    }"
+                    v-on:click="FilterList('municipality')"
+                  >
+                    Local
+                  </v-btn>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <v-btn
+                    :to="{
+                      name: 'Reps',
+                      params: { postalCode: postalCode }
+                    }"
+                    v-on:click="CreateRepList()"
+                    clickclass="mr-4"
+                    >Submit
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+
+          <div id="reprenstatives-list" v-show="hasContent">
+            <div>
+              <v-card flat v-for="member in congressMembers" :key="member.name">
+                <representative-card
+                  @handleRepSelected="handleRepSelected"
+                  :member="member"
                 >
-                  Federal
-                </v-btn>
-
-                <v-btn
-                  class="search-reps-button"
-                  rounded
-                  dark
-                  :class="{ active: isActive }"
-                  :style="{
-                    backgroundColor:
-                      currentFilter === 'state' && isActive ? 'blue' : 'gray'
-                  }"
-                  v-on:click="FilterList('state')"
-                >
-                  State
-                </v-btn>
-
-                <v-btn
-                  class="search-reps-button"
-                  rounded
-                  dark
-                  :class="{ active: isActive }"
-                  :style="{
-                    backgroundColor:
-                      currentFilter === 'county' && isActive ? 'blue' : 'gray'
-                  }"
-                  v-on:click="FilterList('county')"
-                >
-                  County
-                </v-btn>
-
-                <v-btn
-                  rounded
-                  dark
-                  class="ui button toggle search-reps-button"
-                  :style="{
-                    backgroundColor:
-                      currentFilter === 'municipality' && isActive
-                        ? 'blue'
-                        : 'gray'
-                  }"
-                  v-on:click="FilterList('municipality')"
-                >
-                  Local
-                </v-btn>
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col>
-                <v-btn
-                  :to="{
-                    name: 'Reps',
-                    params: { postalCode: postalCode }
-                  }"
-                  v-on:click="CreateRepList()"
-                  clickclass="mr-4"
-                  >Submit
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-
-        <div id="reprenstatives-list" v-show="hasContent">
-          <div>
-            <v-card flat v-for="member in congressMembers" :key="member.name">
-              <representative-card
-                @handleRepSelected="handleRepSelected"
-                :member="member"
-              ></representative-card>
-              <v-divider></v-divider>
-            </v-card>
+                </representative-card>
+                <v-divider></v-divider>
+              </v-card>
+            </div>
           </div>
-        </div>
-      </v-col>
-      <v-divider vertical></v-divider>
-      <v-col>
-        <div v-if="!listVisible">
-          <div>
-            <img
-              alt="Vue logo"
-              src="../assets/images/StepsGraphic.png"
-              width="70%"
-            />
-          </div>
-          <p class="text-h6 pa-10">
-            The bill establishes an interim goal to reduce greenhouse gas
-            emissions to at least 50% below 2005 levels by 2030 as well as a
-            national goal to achieve net-zero greenhouse gas emissions by 2050.
-          </p>
-          <p></p>
-        </div>
+        </v-col>
 
-        <div v-else>
-          <take-action
-            :letterBody="letterBody"
-            :selectedRep="selectedRep"
-          ></take-action>
-        </div>
-      </v-col>
-    </v-row>
+        <v-divider vertical class="hidden-sm-and-down"></v-divider>
+
+        <v-col cols="12" sm="6">
+          <div v-if="!listVisible">
+            <div>
+              <img
+                alt="Vue logo"
+                src="../assets/images/StepsGraphic.png"
+                width="80%"
+              />
+            </div>
+            <p class="text-h6 pa-4">
+              The bill establishes an interim goal to reduce greenhouse gas
+              emissions to at least 50% below 2005 levels by 2030 as well as a
+              national goal to achieve net-zero greenhouse gas emissions by
+              2050.
+            </p>
+            <p></p>
+          </div>
+
+          <div v-else>
+            <take-action
+              :letterBody="letterBody"
+              :selectedRep="selectedRep"
+            ></take-action>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
@@ -142,7 +150,7 @@ export default {
         RepresentativeCard,
         TakeAction
     },
-    data () {
+    data() {
         return {
             letterBody: '',
             selectedRep: {},
@@ -155,7 +163,7 @@ export default {
         }
     },
     methods: {
-        handleRepSelected (letterBody, selectedRep, step2) {
+        handleRepSelected(letterBody, selectedRep, step2) {
             this.letterBody = letterBody
             this.selectedRep = selectedRep
             this.step2 = step2
@@ -167,7 +175,7 @@ export default {
                 this.hasContent = false
             }
         },
-        async CreateRepList () {
+        async CreateRepList() {
             try {
                 const res = await axios.get(
                     '/api/representatives/' + this.postalCode
@@ -182,7 +190,7 @@ export default {
                 console.error(e)
             }
         },
-        async FilterList (level) {
+        async FilterList(level) {
             try {
                 this.currentFilter = level
 
@@ -196,7 +204,7 @@ export default {
                     const res = await axios.get(
                         '/api/representatives/' + this.postalCode,
                         {
-                          params
+                            params
                         }
                     )
 
