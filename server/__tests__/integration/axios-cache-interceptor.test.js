423a5d4bd50053518e7b1e0644795440c0db3ccf
Nathaly Toledo <nathaly12toledo@gmail.com>
2022-11-04 15:54:47 +0000

    added the final test to check the proper invalidation of request with a timeout property

--- server/__tests__/integration/axios-cache-interceptor.test.js
+++ server/__tests__/integration/axios-cache-interceptor.test.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
-// import required dependencies for cache interceptor to work
 const Axios = require('axios')
 const { setupCache } = require('axios-cache-interceptor')
 
-// to test: npm test -- axios-cache-interceptor.test.js
-// 1. we test that the configuration we set is valid and available
+// 1. we test that the configuration we set is valid and available to check
 describe('test Axios configuration', () => {
   test('tests argument composition', () => {
     const axios = Axios.create()
     const withAxios = setupCache(axios)
+    // we expect axios to be properly configured with setupCache
     expect(withAxios).not.toBeUndefined()
 
+    // we expect the cache to be properly configured and contain our properties after initialization
     const withConfig = setupCache(axios, { ttl: 1234 })
     expect(withConfig).not.toBeUndefined()
     expect(withConfig.defaults.cache.ttl).toBe(1234)
@@ -23,17 +23,35 @@ describe('test Axios cache with concurrent requests', () => {
     const axios = Axios.create()
     const cache_interceptor = setupCache(axios)
 
-    // the response is cached. Reference: https://apipheny.io/free-api/
+    // we make two concurrent requests to the same endpoint
     const req1 = cache_interceptor.get('https://api.publicapis.org/entries')
     const req2 = cache_interceptor.get('https://api.publicapis.org/entries/')
-
-    // req1 and req2 are the same promise
     const [res1, res2] = await Promise.all([req1, req2])
 
-    // assertions
+    // assertions: we expect the second response to be cached
     expect(res1.cached).toBe(false)
     expect(res2.cached).toBe(true)
   })
 })
 
 // 3. we test that the cache is invalidated when the ttl expires
+describe('test Axios cache invalidation option', () => {
+  test('tests cache invalidation', async () => {
+    // we set the ttl to 10 seconds
+    const axios = Axios.create()
+    const cache_interceptor = setupCache(axios, { ttl: 10000 })
+    // we make two concurrent requests
+    const req1 = cache_interceptor.get('https://api.publicapis.org/entries')
+    const req2 = cache_interceptor.get('https://api.publicapis.org/entries/')
+    const [res1, res2] = await Promise.all([req1, req2])
+    // assertions: the first is never cached, we expect the second to be cached
+    expect(res1.cached).toBe(false)
+    expect(res2.cached).toBe(true)
+    // we wait for 10.1 seconds and make another call
+    await new Promise((resolve) => setTimeout(() => resolve(), 11000))
+    const req3 = cache_interceptor.get('https://api.publicapis.org/entries')
+    const res3 = await req3
+    // assertions: the third is expected not to be cached, due to the ttl
+    expect(res3.cached).toBe(false)
+  }, 70000)
+})
