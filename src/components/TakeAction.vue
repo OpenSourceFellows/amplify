4b1ebb8cf1f47c4b74df4e59adda5d99007ce613
Tee O <65982182+tdo95@users.noreply.github.com>
2022-11-15 20:19:59 +0000

    fixed formatting

--- src/components/TakeAction.vue
+++ src/components/TakeAction.vue
@@ -1,20 +1,30 @@
 <template lang="html">
   <section class="take-action">
     <v-expansion-panels v-model="panel" flat>
-      <v-expansion-panel :key="0" v-model="panel" >
+      <v-expansion-panel :key="0" v-model="panel">
         <v-expansion-panel-header :disabled="!isActive(0)">
           <template v-slot:actions>
             <v-icon :color="determineStyles('icon', panelStatus[0])" size="45">
               $expand
             </v-icon>
           </template>
-          <v-list-item-avatar :color="determineStyles('avatar', panelStatus[0])" max-width="40px" >
-            <v-icon v-if="panelStatus[0] === 'completed'" class="white--text"> mdi-check-bold </v-icon>
+          <v-list-item-avatar
+            :color="determineStyles('avatar', panelStatus[0])"
+            max-width="40px"
+          >
+            <v-icon v-if="panelStatus[0] === 'completed'" class="white--text">
+              mdi-check-bold
+            </v-icon>
             <span v-else class="text-h5 font-weight-bold white--text"> 1</span>
           </v-list-item-avatar>
           <v-list-item two-line>
             <v-list-item-content>
-              <v-list-item-title class="text-h6 " :class="determineStyles('title', panelStatus[0])"> Review the letter </v-list-item-title>
+              <v-list-item-title
+                class="text-h6"
+                :class="determineStyles('title', panelStatus[0])"
+              >
+                Review the letter
+              </v-list-item-title>
               <v-list-item-subtitle class="text-wrap font-weight-medium">
                 Add any additional details to organizer requests.
               </v-list-item-subtitle>
@@ -44,14 +54,26 @@
               $expand
             </v-icon>
           </template>
-          <v-list-item-avatar class="50px" :color="determineStyles('avatar', panelStatus[1])" max-width="40px">
-            <v-icon v-if="panelStatus[1] === 'completed'" class="white--text"> mdi-check-bold </v-icon>
+          <v-list-item-avatar
+            class="50px"
+            :color="determineStyles('avatar', panelStatus[1])"
+            max-width="40px"
+          >
+            <v-icon v-if="panelStatus[1] === 'completed'" class="white--text">
+              mdi-check-bold
+            </v-icon>
             <span v-else class="text-h5 font-weight-bold white--text"> 2</span>
           </v-list-item-avatar>
           <v-list-item two-line>
             <v-list-item-content>
-              <v-list-item-title class="text-h6" :class="determineStyles('title', panelStatus[1])">Sign your name</v-list-item-title>
-              <v-list-item-subtitle class="font-weight-medium">Use your legal name.</v-list-item-subtitle>
+              <v-list-item-title
+                class="text-h6"
+                :class="determineStyles('title', panelStatus[1])"
+                >Sign your name</v-list-item-title
+              >
+              <v-list-item-subtitle class="font-weight-medium"
+                >Use your legal name.</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
         </v-expansion-panel-header>
@@ -74,13 +96,22 @@
               $expand
             </v-icon>
           </template>
-          <v-list-item-avatar :color="determineStyles('avatar', panelStatus[2])" max-width="40px">
-            <v-icon v-if="panelStatus[2] === 'completed'" class="white--text"> mdi-check-bold </v-icon>
+          <v-list-item-avatar
+            :color="determineStyles('avatar', panelStatus[2])"
+            max-width="40px"
+          >
+            <v-icon v-if="panelStatus[2] === 'completed'" class="white--text">
+              mdi-check-bold
+            </v-icon>
             <span v-else class="text-h5 font-weight-bold white--text"> 3</span>
           </v-list-item-avatar>
           <v-list-item two-line>
             <v-list-item-content>
-              <v-list-item-title class="text-h6" :class="determineStyles('title', panelStatus[2])">Send the letter</v-list-item-title>
+              <v-list-item-title
+                class="text-h6"
+                :class="determineStyles('title', panelStatus[2])"
+                >Send the letter</v-list-item-title
+              >
               <v-list-item-subtitle class="text-wrap font-weight-medium">
                 Postage is $1.50 to send your letter.Learn more about what how
                 this fee is used.
@@ -139,20 +170,20 @@ export default {
         nextPage () {
             const previousPanel = this.panel
             const nextPanel = this.panel + 1
-            
+
             //update previous panel's status
             this.panelStatus[previousPanel] = 'completed'
             //update next panel's status
             this.panelStatus[nextPanel] = 'inProgress'
             //move to next panel
-            this.panel = this.panel += 1  
+            this.panel = this.panel += 1
         },
         isActive(panelNumber) {
             return this.panelStatus[panelNumber] === 'inProgress'
         },
         determineStyles(element, status) {
           if(element === 'icon') {
-            if (status === 'inProgress') return 'theme_darkBlue' 
+            if (status === 'inProgress') return 'theme_darkBlue'
             else return 'grey lighten-1'
           }
           else if (element === 'avatar') {
@@ -177,8 +208,6 @@ export default {
   }
   .theme_darkBlue--text {
     color: @theme_darkBlue;
-  } 
+  }
 }
-
 </style>
-
