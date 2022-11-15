ee4c15852edf6b73dc34ddb752d92e5fd6feb064
Cris <rcmtcristian@gmail.com>
2022-11-15 01:36:23 +0000

    Fix: lob and checkout test were failing.

--- server/__tests__/integration/representatives.test.js
+++ server/__tests__/integration/representatives.test.js
@@ -17,34 +17,41 @@ afterEach(() => {
 afterAll(async () => {
   await new Promise((resolve) => setTimeout(() => resolve(), 500)) // avoid jest open handle error
 })
-describe('GET api/representatives/:zipcode', () => {
-  const zipcode = '20510-1234'
-  const route = `/api/representatives/${zipcode}`
+
+describe('GET  api/representatives/:zipCode', () => {
+  const zipCode = '20510-1234'
+  const route = `/api/representatives/${zipCode}`
 
   const exampleZipResponse = {
-    id: '',
-    name: 'Maria Cantwell',
-    title: 'U.S. Senator',
-    address_line1: '511 Hart Senate Office Building',
-    address_line2: '',
-    address_city: 'Washington',
-    address_state: 'DC',
-    address_zip: zipcode,
-    address_country: 'US',
-    email: 'Not Made Public',
-    contactPage: '',
-    photoUrl:
-      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
-    socialMediaPages: 'SenatorCantwell',
-    photoCroppingCSS: 'center center'
+    errors: [],
+    messages: [],
+    results: {
+      candidates: [
+        {
+          count: {},
+          officials: [],
+          locator: 'World',
+          score: 100,
+          match_addr: ` ${zipCode}, Philadelphia, Pennsylvania`,
+          x: -75.15263499999998,
+          y: 39.93611000000004,
+          wkid: 4326,
+          locator_type: 'Postal',
+          geoservice: 'EsriWGS',
+          match_city: 'Philadelphia',
+          match_subregion: 'Philadelphia County',
+          match_region: 'PA',
+          match_postal: zipCode,
+          match_country: 'US',
+          match_streetaddr: ''
+        }
+      ]
+    }
   }
-
   test('returns 200 status for a a successful mock enpoint', async () => {
     const spy = jest.spyOn(axios, 'get')
     spy.mockImplementation((url) => {
-      if (
-        url === `${CICERO_API_HOST}/api/representatives/${zipcode.slice(0, 5)}`
-      ) {
+      if (url === `${CICERO_API_HOST}?search_postal=${zipCode.slice(0, 5)}`) {
         return {
           status: 200,
 
@@ -58,37 +65,80 @@ describe('GET api/representatives/:zipcode', () => {
     expect(spy).toHaveBeenCalled()
     spy.mockRestore()
   })
-  //  test('returns 200 status for a a successful mock enpoint', async () => {
-  //     const spy = jest.spyOn(axios, 'get')
-  //     spy.mockImplementation((url) => {
-  //       if (url === `/api/representatives/${zipcode}`) {
-  //         return {
-  //         status: 200,
-  //         data: exampleZipResponse
-  //       }
-  //       }
-  //     })
-  //     const response = await request(app).get(route)
-  //     expect(response.status).toBe(200)
-
-  //     expect(spy).toHaveBeenCalled()
-  //     spy.mockRestore()
-  //   })
-  //  test('returns 200 status for a failed mock enpoint', async () => {
-  //     const spy = jest.spyOn(axios, 'get')
-  //     spy.mockImplementation((url) => {
-  //       if (url !== `/api/representatives/${zipcode}`) {
-  //         throw new Error(`Invalid zip code format, valid examples are 84054-6013 or 84054. The zipcode used was ${zipcode}` )
-  //       }
-  //       return {
-  //         status: 400,
-  //       }
-  //     })
-
-  //     const response = await request(app).get(route)
-  //     expect(response.status).toBe(400)
-
-  //     expect(spy).toHaveBeenCalled()
-  //     spy.mockRestore()
-  //   })
 })
+
+// describe('GET api/representatives/:zipcode', () => {
+//   const zipcode = '20510-1234'
+//   const route = `/api/representatives/${zipcode}`
+
+//   const exampleZipResponse = {
+//     id: '',
+//     name: 'Maria Cantwell',
+//     title: 'U.S. Senator',
+//     address_line1: '511 Hart Senate Office Building',
+//     address_line2: '',
+//     address_city: 'Washington',
+//     address_state: 'DC',
+//     address_zip: zipcode,
+//     address_country: 'US',
+//     email: 'Not Made Public',
+//     contactPage: '',
+//     photoUrl:
+//       'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+//     socialMediaPages: 'SenatorCantwell',
+//     photoCroppingCSS: 'center center'
+//   }
+
+//   test('returns 200 status for a a successful mock enpoint', async () => {
+//     const spy = jest.spyOn(axios, 'get')
+//     spy.mockImplementation((url) => {
+//       if (
+//         url === `${CICERO_API_HOST}/api/representatives/${zipcode.slice(0, 5)}`
+//       ) {
+//         return {
+//           status: 200,
+
+//           data: { exampleZipResponse }
+//         }
+//       }
+//     })
+//     const response = await request(app).get(route)
+//     expect(response.status).toBe(200)
+
+//     expect(spy).toHaveBeenCalled()
+//     spy.mockRestore()
+//   })
+//  test('returns 200 status for a a successful mock enpoint', async () => {
+//     const spy = jest.spyOn(axios, 'get')
+//     spy.mockImplementation((url) => {
+//       if (url === `/api/representatives/${zipcode}`) {
+//         return {
+//         status: 200,
+//         data: exampleZipResponse
+//       }
+//       }
+//     })
+//     const response = await request(app).get(route)
+//     expect(response.status).toBe(200)
+
+//     expect(spy).toHaveBeenCalled()
+//     spy.mockRestore()
+//   })
+//  test('returns 200 status for a failed mock enpoint', async () => {
+//     const spy = jest.spyOn(axios, 'get')
+//     spy.mockImplementation((url) => {
+//       if (url !== `/api/representatives/${zipcode}`) {
+//         throw new Error(`Invalid zip code format, valid examples are 84054-6013 or 84054. The zipcode used was ${zipcode}` )
+//       }
+//       return {
+//         status: 400,
+//       }
+//     })
+
+//     const response = await request(app).get(route)
+//     expect(response.status).toBe(400)
+
+//     expect(spy).toHaveBeenCalled()
+//     spy.mockRestore()
+//   })
+//})
