d534f72680edb38d1fc1c6be5fe1cf83c230df16
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-02-04 20:51:07 +0000

    Fix to formatting for scroll-behavior file.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import SearchReps from '../components/SearchReps'
 import RepresentativeCard from '../components/RepresentativeCard'
+import scrollBehavior from './scroll-behavior'
+import App from '/workspace/src/App.vue'
 // import Reps from '../components/Reps'
 
 Vue.use(VueRouter)
@@ -48,8 +50,14 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
+  scrollBehavior,
   base: process.env.BASE_URL,
   routes
 })
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
 
 export default router
