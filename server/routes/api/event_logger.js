0cd0e2d89550f4b320910e0177aa8be085e2aa7e
Rachel Sensenig <94768032+rsensenig@users.noreply.github.com>
2023-02-17 21:53:41 +0000

    moved utilities folder into the server folder

--- server/routes/api/event_logger.js
+++ server/routes/api/event_logger.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const logger = require('../../../utilities/logger')
+const logger = require('../../utilities/logger')
 
 router.post('/log', (req, res) => {
   const { data } = req.body
