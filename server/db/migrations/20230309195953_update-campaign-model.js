3139b91cd61c3a2a2310afadb638f24396898ec2
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-09 20:13:48 +0000

    create new migration for updating campaign model

---
+++ server/db/migrations/20230309195953_update-campaign-model.js
@@ -0,0 +1,20 @@
+const tableName = 'campaigns'
+
+module.exports = {
+  async up(knex) {
+    await knex.schema.createTable(tableName, (table) => {
+      // Auto-incrementing non-nullable unsigned integer primary key "id" field
+      table.increments()
+
+      // Simple fields
+      table.string('campaign_tagline').notNullable()
+      table.string('campaign_text').notNullable()
+      table.string('supplemental_text').nullable()
+    })
+  },
+
+  async down(knex) {
+    // Drop the table
+    await knex.schema.dropTable(tableName)
+  }
+}
