19b7f75b1c73428745641950e9ca53dc8aaab0d2
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-11 18:19:17 +0000

    removal of incrementing and change of simple fields to nullable

--- server/db/migrations/20230309195953_update-campaign-model.js
+++ server/db/migrations/20230309195953_update-campaign-model.js
@@ -3,12 +3,9 @@ const tableName = 'campaigns'
 module.exports = {
   async up(knex) {
     await knex.schema.alterTable(tableName, (table) => {
-      // Auto-incrementing non-nullable unsigned integer primary key "id" field
-      table.increments()
-
       // Simple fields
-      table.string('campaign_tagline').notNullable()
-      table.string('campaign_text').notNullable()
+      table.string('campaign_tagline').nullable()
+      table.string('campaign_text').nullable()
       table.string('supplemental_text').nullable()
     })
   },
