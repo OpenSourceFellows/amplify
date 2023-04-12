81198bcb07d1ae12e39d17a938f48da6343f448e
Teri Eyenike <teyenike1@gmail.com>
2023-04-12 15:05:26 +0000

    Update: prettier

--- server/auth/check-jwt.js
+++ server/auth/check-jwt.js
@@ -18,24 +18,3 @@ const checkJwt = jwt({
 module.exports = {
   checkJwt
 }
-
-// const jwt = require('express-jwt')
-// const jwksRsa = require('jwks-rsa')
-// const { domain, audience } = require('./config/env.dev')
-
-// const checkJwt = jwt({
-//   secret: jwksRsa.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: `https://${domain}/.well-known/jwks.json`
-//   }),
-
-//   audience: audience,
-//   issuer: `https://${domain}/`,
-//   algorithms: ['RS256']
-// })
-
-// module.exports = {
-//   checkJwt
-// }
