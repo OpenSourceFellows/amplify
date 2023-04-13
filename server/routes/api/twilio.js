b1073ac4ebafba089baa1054b27d77f543890432
David Chapuis <85941388+davidchapuis@users.noreply.github.com>
2023-04-13 09:09:42 -0300

    Update twilio.js

--- server/routes/api/twilio.js
+++ server/routes/api/twilio.js
@@ -5,9 +5,9 @@ const { createClient } = require('../../db')
 const router = express.Router()
 const db = createClient()
 // // need to `npm install --save twilio` first
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilio = require('twilio')(accountSid, authToken);
+const twilio = require('twilio')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
 
 
 module.exports = router
