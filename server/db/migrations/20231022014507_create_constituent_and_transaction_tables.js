50865ffcb89ad0620e01a6c1b0dd3c4e684da4c8
Glenn Piludu <gpiludu@gmail.com>
2023-10-22 08:53:53 -0500

    Fixed db migration to UPDATE already existing constituent and transactions tables

--- server/db/migrations/20231022014507_create_constituent_and_transaction_tables.js
+++ server/db/migrations/20231022014507_create_constituent_and_transaction_tables.js
@@ -1,32 +1,21 @@
 module.exports = {
   async up(knex) {
-    await knex.schema.createTable('constituents', (table) => {
-      table.increments('id')
+    await knex.schema.alterTable('constituents', (table) => {
       table.timestamps()
-      table.string('first_name')
-      table.string('last_name')
-      table.string('address_line_1')
-      table.string('address_line_2')
-      table.string('city')
-      table.string('state')
-      table.string('zipcode')
-      table.string('email')
+      table.renameColumn('street_address', 'address_line_1')
+      table.renameColumn('address_two', 'address_line_2')
     })
 
     // TODO: payment status id table
 
-    await knex.schema.createTable('transactions', (table) => {
-      table.increments('id')
-      table.timestamps()
-      table.string('stripe_transaction_id')
+    await knex.schema.alterTable('transactions', (table) => {
       table.integer('constituent_id')
-      table.integer('amount')
-      table.string('currency')
-      table.string('status_id')
+      table.foreign('constituent_id').references('constituents.id')
     })
   },
 
   async down(knex) {
-    await knex.schema.doSomethingForReal()
+    await knex.schema.dropTable('constituents')
+    await knex.schema.dropTable('transactions')
   }
 }
