034119862e71c87c588c2db3d032efc0595118e8
Daniel Chan <101846403+danchan020@users.noreply.github.com>
2023-10-27 01:04:28 +0000

    post req

---
+++ server/routes/api/v1/campaigns.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const Campaign = require('../../../db/models/campaign')
+
+router.post('/v1/campaigns', async (req, res) => {
+  try {
+    const addCampaign = new Campaign(req.body)
+    await addCampaign.save()
+    res.status(201).json(addCampaign)
+  } catch (error){
+    console.error(error)
+    res.status(500).json({error: 'Internal Server Error'})
+  }
+  })
+
+
+module.exports = router
