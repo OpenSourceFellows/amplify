e6a334ed0678dc88f817411983cd94c212dad65d
Glenn Piludu <gpiludu@gmail.com>
2023-11-05 15:01:28 -0600

    more db stuff

--- server/db/models/_base.js
+++ server/db/models/_base.js
@@ -1,9 +1,16 @@
-const { Model } = require('objection')
+const { Model, snakeCaseMappers } = require('objection')
 const { createClient } = require('../')
 
 class BaseModel extends Model {
   static get modelPaths() {
     return [__dirname]
+  }
+
+  // Maps column_name in postgres to columnName in the app.
+  // Queries must still use snake_case if sql is passed into a query method chain
+  // For ex. Constituent.query().where('first_name', 'Jennifer')
+  static get columnNameMappers() {
+    return snakeCaseMappers()
   }
 }
 
