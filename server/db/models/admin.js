1d56b6c7b9910ea4c443ac4465d1ed4d25ccbd0c
e.wang <91093545+E-wang42@users.noreply.github.com>
2023-11-05 16:29:14 +0000

    update

--- server/db/models/admin.js
+++ server/db/models/admin.js
@@ -1,6 +1,6 @@
 const BaseModel = require('./_base')
 
-const emailRegex = `^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$`
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 
 class Admin extends BaseModel {
   static get tableName() {
@@ -20,12 +20,12 @@ class Admin extends BaseModel {
         first_name: { type: 'string', minLength: 1, maxLength: 255 },
         last_name: { type: 'string', minLength: 1, maxLength: 255 },
         email: {
-            type: 'string', 
-            format: 'email',
-            pattern: emailRegex,
-            minLength: 6, 
-            maxLength: 127
-          },
+          type: 'string',
+          format: 'email',
+          pattern: emailRegex,
+          minLength: 6,
+          maxLength: 127
+        },
         password: { type: 'string', minLength: 8 },
         active: { type: 'boolean' }
       }
