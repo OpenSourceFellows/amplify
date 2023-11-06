cd65b643bfa4b3e72109d9fc4e1afe3e2dd5f119
Cocoa Wang <wangshangguan@hotmail.com>
2023-11-06 01:13:30 +0000

    create PUT request for campaign endpoint for #615

--- server/routes/api/v1/campaigns/index.js
+++ server/routes/api/v1/campaigns/index.js
@@ -36,4 +36,27 @@ router.get('/', async (req, res) => {
   }
 })
 
+// PUT request to update a campaign
+router.put('/:id', async (req, res) => {
+  const campaignId = req.params.id
+
+  if (!campaignId) return res.status(404).end()
+
+  try {
+    const updatedCampaign = await Campaign.query().patchAndFetchById(
+      campaignId,
+      req.body.campaign
+    )
+
+    if (!updatedCampaign)
+      return res.status(404).json({ msg: 'Campaign not found' }).end()
+
+    return res.status(200).json({ campaign: updatedCampaign }).end()
+  } catch (error) {
+    console.error(error.message)
+
+    return res.status(500).json({ msg: error.message }).end()
+  }
+})
+
 module.exports = router
