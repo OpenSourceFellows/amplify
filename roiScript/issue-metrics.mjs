b805fb28ddd6dbe458e271e1992e6a85ab0f7670
Maksym Polinka <Polinka7max@gmail.com>
2023-11-07 15:58:17 -0800

    Update issue-metrics.mjs

--- roiScript/issue-metrics.mjs
+++ roiScript/issue-metrics.mjs
@@ -2,47 +2,47 @@
 
 console.log('test'); // works
 
-import * as core from '@actions/core';
-import github from '@actions/github';
+// import * as core from '@actions/core';
+// import github from '@actions/github';
 
-// const core = require('@actions/core');
-// const github = require('@actions/github');
+// // const core = require('@actions/core');
+// // const github = require('@actions/github');
 
-const assignedIssues = {};
+// const assignedIssues = {};
 
-const main = async () => {
-  const payload = JSON.parse(core.getInput('payload'));
+// const main = async () => {
+//   const payload = JSON.parse(core.getInput('payload'));
 
-  if (payload.action === 'assigned') {
-    const issue = payload.issue;
-    assignedIssues[issue.number] = issue.assignee.login;
-  }
+//   if (payload.action === 'assigned') {
+//     const issue = payload.issue;
+//     assignedIssues[issue.number] = issue.assignee.login;
+//   }
 
-  if (payload.action === 'unlabeled' && payload.label.name === 'assigned') {
-    delete assignedIssues[payload.issue.number];
-  }
+//   if (payload.action === 'unlabeled' && payload.label.name === 'assigned') {
+//     delete assignedIssues[payload.issue.number];
+//   }
 
-  if (payload.action === 'opened') {
-    const pull = payload.pull_request;
-    const issueNumber = pull.title.split('#')[1];
+//   if (payload.action === 'opened') {
+//     const pull = payload.pull_request;
+//     const issueNumber = pull.title.split('#')[1];
 
-    if (issueNumber && assignedIssues[issueNumber]) {
-      const timeDelta = new Date(pull.created_at) - new Date(assignedIssues[issueNumber]);
+//     if (issueNumber && assignedIssues[issueNumber]) {
+//       const timeDelta = new Date(pull.created_at) - new Date(assignedIssues[issueNumber]);
 
-      const comment = `Time from assignment to PR for #${issueNumber}: ${timeDelta} ms`;
+//       const comment = `Time from assignment to PR for #${issueNumber}: ${timeDelta} ms`;
 
-      const octokit = github.getOctokit(core.getInput('github-token'));
+//       const octokit = github.getOctokit(core.getInput('github-token'));
 
-      await octokit.issues.createComment({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: pull.number,
-        body: comment,
-      });
-    }
-  }
-};
+//       await octokit.issues.createComment({
+//         owner: github.context.repo.owner,
+//         repo: github.context.repo.repo,
+//         issue_number: pull.number,
+//         body: comment,
+//       });
+//     }
+//   }
+// };
 
-main().catch((error) => {
-  core.setFailed(error.message);
-});
+// main().catch((error) => {
+//   core.setFailed(error.message);
+// });
