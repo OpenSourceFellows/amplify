8613a531370228bb30dc1fb72b129408837eec1f
Cocoa Wang <wangshangguan@hotmail.com>
2023-11-09 05:55:25 +0000

    run prettier on campaign PUT test script

--- server/__tests__/unit/campaign_put_endpoint.test.js
+++ server/__tests__/unit/campaign_put_endpoint.test.js
@@ -1,38 +1,35 @@
 const request = require('supertest')
 const app = require('../../app')
-const Campaign = require('../../db/models/campaign');
+const Campaign = require('../../db/models/campaign')
 
-jest.mock('../../db/models/campaign');
-
+jest.mock('../../db/models/campaign')
 
 describe('PUT /api/v1/campaigns/:id', () => {
   it('should return 200 if successfully updated the campaign', async () => {
-      const campaignData = {
-        id: 5,
-        name: "Sogorea 'Te Land Trust",
-        organization: "Sogorea 'Te Land Trust",
-        page_url: "https://sogoreate-landtrust.org/",
-        cause: "Civic Rights",
-        type: "Grant"
-      };
-      // Mock patchAndFetchById to return a resolved promise
-      Campaign.query.mockReturnValue({
-        patchAndFetchById: jest.fn().mockResolvedValue(campaignData)
-      });
-    const response = await request(app)
-      .put('/api/v1/campaigns/5')
-      .send({
-        campaign: campaignData
-      })
+    const campaignData = {
+      id: 5,
+      name: "Sogorea 'Te Land Trust",
+      organization: "Sogorea 'Te Land Trust",
+      page_url: 'https://sogoreate-landtrust.org/',
+      cause: 'Civic Rights',
+      type: 'Grant'
+    }
+    // Mock patchAndFetchById to return a resolved promise
+    Campaign.query.mockReturnValue({
+      patchAndFetchById: jest.fn().mockResolvedValue(campaignData)
+    })
+    const response = await request(app).put('/api/v1/campaigns/5').send({
+      campaign: campaignData
+    })
     expect(response.status).toBe(200)
-    expect(response.body.campaign).toEqual(campaignData);
+    expect(response.body.campaign).toEqual(campaignData)
   })
 
   it('should return 400 if no campaign is found', async () => {
     // Mock patchAndFetchById to return undefined
     Campaign.query.mockReturnValue({
       patchAndFetchById: jest.fn().mockResolvedValue(undefined)
-    });
+    })
     const response = await request(app)
       .put('/api/v1/campaigns/999999')
       .send({ campaign: { type: 'Accelerator' } })
@@ -44,8 +41,8 @@ describe('PUT /api/v1/campaigns/:id', () => {
   it('should return 500 for invalid update', async () => {
     // Mock patchAndFetchById to throw an error
     Campaign.query.mockReturnValue({
-    patchAndFetchById: jest.fn().mockRejectedValue(new Error('Server error'))
-    });
+      patchAndFetchById: jest.fn().mockRejectedValue(new Error('Server error'))
+    })
     const response = await request(app)
       .put('/api/v1/campaigns/5')
       .send({
@@ -57,4 +54,3 @@ describe('PUT /api/v1/campaigns/:id', () => {
     expect(response.status).toBe(500)
   })
 })
-
