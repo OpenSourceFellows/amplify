6a387e61da27d97d6c546682862c7d7472542492
Maksym Polinka <Polinka7max@gmail.com>
2023-11-20 14:19:42 -0800

    Create log-branch.mjs

---
+++ roiScript/log-branch.mjs
@@ -0,0 +1,14 @@
+import github from '@actions/github'
+
+const issueNumber = process.env.ISSUE_NUMBER
+const timeDiff = process.env.TIME_DIFF ? process.env.TIME_DIFF : 1
+const comment = `Branch: [issue-${issueNumber}](https://github.com/ProgramEquity/amplify/tree/issue-${issueNumber})`
+
+const octokit = github.getOctokit(process.env.GH_TOKEN)
+
+await octokit.rest.issues.createComment({
+  owner: github.context.repo.owner,
+  repo: github.context.repo.repo,
+  issue_number: issueNumber,
+  body: comment
+})
