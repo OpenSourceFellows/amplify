263c72c998007eb88fe5624495f59a77b56de902
Maye Jesuorobo <99496129+CodingLady22@users.noreply.github.com>
2023-12-04 21:23:20 +0000

    Delete extra comments and commented out codes

--- server/routes/api/v1/v1.js
+++ server/routes/api/v1/v1.js
@@ -1,13 +1,8 @@
 const express = require('express')
-const campaigns = require('./campaigns') // If the routes file is named index.js, we can omit it when we do the import instead of writing './campaigns/index'.
-// const { exampleMiddleware } = require('../../../middleware/example')
+const campaigns = require('./campaigns')
 
 const v1Router = express.Router()
 
-// Use Middleware
-// v1Router.use(exampleMiddleware)
-
-// Create the final piece of the url /campaigns
 v1Router.use('/campaigns', campaigns)
 
 module.exports = v1Router
