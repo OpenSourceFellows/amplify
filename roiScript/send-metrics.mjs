2d72d0950e54b8c204f2d210974e69b3c1f48e3c
Maksym Polinka <Polinka7max@gmail.com>
2023-12-05 15:32:31 -0800

    Update send-metrics.mjs

--- roiScript/send-metrics.mjs
+++ roiScript/send-metrics.mjs
@@ -1,2 +1,36 @@
+const { Client } = require('@notionhq/client');
 
-console.log('Sending metrics to Notion DB...')
+console.log('entered the file');
+// TODO: test and move to the secrets in the repo
+// integration key 
+const NOTION_TOKEN=secret_KFNUHq8rmiuo5mQqcGlYsUCIaOdHt6rYuGkkPnflwqZ
+// database id 
+const NOTION_DATABASE_ID=cbfc4e41617b4216b9c307222233e316
+
+const notion = new Client({
+  auth: NOTION_TOKEN,
+});
+
+const databaseId = NOTION_DATABASE_ID;
+
+// mock data to test the connection 
+const newData = {
+  issue_id: 2,
+  gh_handle: 'Dunridge',
+  duration: 15
+};
+
+async function addToNotionDatabase() {
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: databaseId,
+      },
+      properties: newData,
+    });
+  } catch (error) {
+    console.error('Error adding data to Notion:', error);
+  }
+}
+
+addToNotionDatabase();
