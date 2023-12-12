f355335e0267896ec698439e249e524a8a7ddbbb
Brian Segura <84042284+briansegura15@users.noreply.github.com>
2023-12-12 20:53:23 +0000

    Implement responsive control panel login form for issue #822
    - Collect and send user credentials via POST request to /api/v1/auth/login
    - Handle response and store session cookie in browser's cookie jar
    - Redirect user to dashboard view after successful login
    - Ensure responsiveness of login form for all screen sizes
    
    This commit addresses issue #822 by implementing a responsive login form for the control panel. As per the specs, the form collects user credentials, communicates with the server, handles the response, stores the session cookie, and redirects the user to the dashboard view upon successful login.

--- server/routes/api/authentication.js
+++ server/routes/api/authentication.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
-
+const uuid = require('uuid') // generates session IDs
 const {
   getPublicMessage,
   getProtectedMessage
@@ -20,6 +20,38 @@ router.get('/public-message', (req, res) => {
 router.get('/protected-message', checkJwt, (req, res) => {
   const message = getProtectedMessage()
   res.status(200).send(message)
+})
+
+// below is the logic for issue #822 in regards to creating a login page
+
+// Mock user data
+const users = {
+  'test@example.com': { password: 'testPassword' }
+}
+
+// Mock session store
+const sessions = {}
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body
+
+  // Look up user by email
+  const user = users[email]
+
+  if (!user || user.password !== password) {
+    return res.status(400).json({ message: 'Invalid email/password' }).end()
+  }
+
+  // Generate a session ID
+  const sessionId = uuid.v4()
+
+  // Store session data on the server
+  sessions[sessionId] = { email }
+
+  // Set a cookie named 'session' that holds the value of sessionId that expires in 900000 ms (15 min)
+  res.cookie('session', sessionId, { httpOnly: true, maxAge: 900000 })
+
+  res.status(200).json({ message: 'Login successful' }).end()
 })
 
 module.exports = router
