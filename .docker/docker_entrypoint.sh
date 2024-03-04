cf5a405f93f8f5c6cf5c931e167eee584e0956eb
Glenn Piludu <gpiludu@gmail.com>
2024-03-03 22:40:41 -0600

    Removed docker_entrypoint.sh because it wasn't running like expected and is unncessary.

--- .docker/docker_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-# Setup the database, if not already done
-echo "Starting container setup..."
-echo "Running database migrations, if there are any."
-npm run db:create
-
-echo "Database setup complete!"
-echo "Container is ready for use!"
