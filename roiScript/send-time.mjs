615876ce15691009b0b8f5d37d8de51a3155a8b2
Maksym Polinka <Polinka7max@gmail.com>
2024-03-12 22:17:20 -0700

    Update send-time.mjs

--- roiScript/send-time.mjs
+++ roiScript/send-time.mjs
@@ -16,4 +16,8 @@ labels interface (filtered labels that contain `originaltime-`):
 */
 
 const labels = process.env.LABELS
-console.log('Send time: ', labels)
+const NOTION_TOKEN = process.env.NOTION_TOKEN
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID
+console.log('Send time (labels): ', labels)
+console.log('NOTION_TOKEN: ', labels)
+console.log('NOTION_DATABASE_ID: ', labels)
