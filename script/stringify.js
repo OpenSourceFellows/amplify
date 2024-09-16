da1076897bd5a8a11534217923bb2c8614193a6e
Glenn <gpiludu@gmail.com>
2024-09-15 21:57:05 -0500

    Onboarding for hakamwe campaign

--- script/stringify.js
+++ script/stringify.js
@@ -1,151 +1,39 @@
 const reps = JSON.stringify([
   {
-    name: 'Connie Chan',
-    title: 'District 1 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Connie_Chan_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'chanstaff@sfgov.org'
-  },
-  {
-    name: 'Catherine Stefani',
-    title: 'District 2 Supervisor',
+    name: 'Dr. Buu Nygren',
+    title: 'President of the Navajo Nation',
     photoUrl:
-      'https://sfbos.org/sites/default/files/Supervisor_Stefani_2018.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
+      'https://i0.wp.com/opvp.navajo-nsn.gov/wp-content/uploads/2023/12/photo_P_Nygren.jpg?resize=1080%2C1080&ssl=1',
+    address_line1: 'PO Box 7440',
+    address_line2: '',
+    address_city: 'Window Rock',
+    address_state: 'AZ',
+    address_zip: '86515',
     address_country: 'US',
-    email: 'Catherine.Stefani@sfgov.org'
+    email: 'alray.nelson@navajo-nsn.gov'
   },
   {
-    name: 'Aaron Peskin',
-    title: 'District 3 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Aaron_Peskin_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'Aaron.Peskin@sfgov.org'
-  },
-  {
-    name: 'Joel Engardio',
-    title: 'District 4 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Joel_Engardio_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: ''
-  },
-  {
-    name: 'Dean Preston',
-    title: 'District 5 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Dean_Preston_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'prestonstaff@sfgov.org'
-  },
-  {
-    name: 'Matt Dorsey',
-    title: 'District 6 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Dorsey_2022_lg.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'DorseyStaff@sfgov.org'
-  },
-  {
-    name: 'Myrna Melgar',
-    title: 'District 7 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Myrna_Melgar_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'melgarstaff@sfgov.org'
-  },
-  {
-    name: 'Rafael Mandelman',
-    title: 'District 8 Supervisor',
+    name: 'Richelle Montoya',
+    title: 'Vice President of the Navajo Nation',
     photoUrl:
-      'https://sfbos.org/sites/default/files/Supervisor_Mandelman_2018.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
+      'https://i0.wp.com/opvp.navajo-nsn.gov/wp-content/uploads/2023/12/photo_P_Nygren.jpg?resize=1080%2C1080&ssl=1',
+    address_line1: 'PO Box 7440',
+    address_line2: '',
+    address_city: 'Window Rock',
+    address_state: 'AZ',
+    address_zip: '86515',
     address_country: 'US',
-    email: 'mandelmanstaff@sfgov.org'
-  },
-  {
-    name: 'Hillary Ronen',
-    title: 'District 9 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Supervisor_Ronen_2019.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'RonenStaff@sfgov.org'
-  },
-  {
-    name: 'Shamann Walton',
-    title: 'District 10 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Shamann_Walton_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'waltonstaff@sfgov.org'
-  },
-  {
-    name: 'Ahsha Safai',
-    title: 'District 11 Supervisor',
-    photoUrl: 'https://sfbos.org/sites/default/files/Ahsha_Safai_2023.jpg',
-    address_line1: '1 Dr Carlton B Goodlett Pl',
-    address_line2: '#244',
-    address_city: 'San Francisco',
-    address_state: 'CA',
-    address_zip: '94102',
-    address_country: 'US',
-    email: 'Ahsha.Safai@sfgov.org'
+    email: 'josie.bowman@navajo-nsn.gov'
   }
 ])
 
 const assets = JSON.stringify({
-  campaign_logo:
-    'https://images.squarespace-cdn.com/content/v1/5eebc0039b04b54b2fb0ce52/1598500758914-E5HAIIGCP0ZXKKMN2FT0/TRT+Logo-13.png?format=500w',
-  campaign_background:
-    'https://images.squarespace-cdn.com/content/v1/5eebc0039b04b54b2fb0ce52/1610135975708-FV42Q30BPWF887M05O51/Poppies-13.jpg?format=1500w',
-  'campaign-img-1':
-    'https://images.squarespace-cdn.com/content/v1/5eebc0039b04b54b2fb0ce52/1609785926325-63M9MY247ORIL8ON8P5E/tuolumne-camp-960x540.jpg?format=1500w',
-  'campaign-img-2':
-    'https://images.squarespace-cdn.com/content/v1/5eebc0039b04b54b2fb0ce52/1592586646372-TBY3WC065NJ04KT2B38Z/000091400001.jpg?format=1500w',
-  'campaign-img-3':
-    'https://images.squarespace-cdn.com/content/v1/5eebc0039b04b54b2fb0ce52/1609781359705-Q5YNFL6DET5ORHW62ZU7/IMG_2249_WEBSITE+copy.jpg?format=1500w'
+  campaign_logo: 'https://i.imgur.com/o3lxGJD.png',
+  campaign_background: 'https://i.imgur.com/qGOHdiP.jpeg',
+  'campaign-img-1': 'https://i.imgur.com/h88GBNC.jpeg',
+  'campaign-img-2': 'https://i.imgur.com/lcQWK6p.jpeg',
+  'campaign-img-3': 'https://i.imgur.com/bnPQVOR.jpeg',
+  'infographic': 'https://i.imgur.com/BJCPDCR.jpeg'
 })
 
 console.log(reps)
