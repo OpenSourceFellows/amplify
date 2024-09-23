cb7c9e7a1d93493545e08eb289c993f540717b8c
David Chapuis <dah.chapuis@gmail.com>
2024-09-23 20:08:59 -0300

    Kaimichi River Onboarding

--- .devcontainer/on-create-command.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# Copy our welcome message
-if [ -f "./.devcontainer/first-run-notice.txt" ]; then
-  sudo cp --force ./.devcontainer/first-run-notice.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
-fi
---
+++ insert_campaign.sql
@@ -0,0 +1,29 @@
+INSERT INTO campaigns (
+    organization,
+    name,
+    cause,
+    type,
+    page_url,
+    logo_url,
+    letters_goal,
+    donation_goal,
+    campaign_tagline,
+    representatives,
+    assets,
+    campaign_text,
+    supplemental_text
+) VALUES (
+    'Kaimichi River Legacy Alliance',
+    'Save the Kaimichi River',
+    'Civic Rights',
+    'Grant',
+    'http://www.kiamichiriver.org/wp/',
+    NULL,  -- logo_url (set to NULL)
+    NULL,  -- letters_goal (set to NULL)
+    NULL,  -- donation_goal (set to NULL)
+    'Save the Kiamichi River and its tributaries from being diverted to Oklahoma City',
+    '[{"name":"Debbie-Anne A. Reese","title":"Acting Secretary","photoUrl":"https://ferc.gov/sites/default/files/styles/gallery_teaser/public/2021-11/ADDDSC_3327_1.jpg","address_line1":"888 First Street NE","address_line2":"Room 1A","address_city":"Washington","address_state":"DC","address_zip":"20426","address_country":"US","email":""}]',
+    '{"campaign_logo":"https://www.guidestar.org/ViewEdoc.aspx?eDocId=10693593&approved=True","campaign_background":"https://www.wildlifedepartment.com/sites/default/files/styles/x_large_16_9_1920x1080_/public/2021-10/KiamichiRiver_KellyAdams.jpg.webp","campaign-img-1":"http://www.kiamichiriver.org/wp/wp-content/gallery/river/18301788_286630255118007_8499792028528706701_n.jpg","campaign-img-2":"http://www.kiamichiriver.org/wp/wp-content/gallery/river/26805188_10210862905795719_2842234758416483368_n.jpg","campaign-img-3":"http://www.kiamichiriver.org/wp/wp-content/gallery/river/26991598_552853938382258_3151811306988112886_n.jpg"}',
+    '<p>Our mission is to promote awareness, conservation, and protection of the waters of the Kiamichi River Basin by the following:</p><p>Promoting interest in, and the wise management of, sound water resource practices and conservation of the waters of the Kiamichi River Basin.</p><p>Providing awareness to the public for the need to preserve and protect the Kiamichi River Basin’s water resources and the environment, habitat, ecosystem, agriculture, economy and local beneficial uses related to the water resources as well as distributing educational materials in connection therewith.</p><p>Ensuring the preservation of the quality and quantity of the Kiamichi River Basin’s water resources and the ecosystem it supports for future generations.</p><p>Promoting the interest of the public in the conservation and drought protection of all water resources of the Kiamichi River Basin and its tributaries.</p><p>Supporting educational programs, scientific studies and environmental research to aid in the sound management of water resources and the conservation of the Kiamichi River Basin and its tributaries.</p><p>Performing any and all other acts necessary or expedient for the administration of the affairs and attainment of the purpose of the corporation.</p>',
+    '<p>Save the Kiamichi River and its tributaries from being diverted to Oklahoma City. Oklahoma Water Resources Board had failed to provide adequate notice about their permitting process. The group believes the water diversion permit, which resulted from the 2016 settlement agreement, relied on faulty science. With more notice, the seven southeast Oklahoma residents feel they could have addressed this issue before the permit was granted.</p>'
+);
